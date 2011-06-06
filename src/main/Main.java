b1617046e15d9183716c741a70be30d2cba06c1b
Oleg Krupenya <taipan90@rambler.ru>
2011-06-06 16:19:25 +0300

    Main class was changed

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        int a = 10;
+        int b = 987;
+        System.out.println(a*b);
     }
 }
