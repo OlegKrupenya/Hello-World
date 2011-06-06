dc16e1553381e6f1ee22819cd177c3d2567245cd
OlegKrupenya <taipan90@rambler.ru>
2011-06-06 06:22:25 -0700

    Edited src/main/Main.java via GitHub

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 987;
-        System.out.println(a*b);
+        System.out.println(a*b+10);
     }
 }
